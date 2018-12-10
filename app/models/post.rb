893edceff11698aba014a8dedd8bc53aef2b1fda
Kei-Tachibana <kei5kei@icloud.com>
2018-12-10 21:10:29 +0900

    Set routing for users, posts & photos

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :photos
+  has_many :photos, dependent: :destroy #Postが削除されるとphotoも消える
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy #ユーザーが削除されるとpostsも消える
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
