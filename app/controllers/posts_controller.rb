c5c8e7bbc3bded9dba8c417287df1dc8b681bcb1
Kei-Tachibana <un.wackeik@gmail.com>
2018-12-23 21:08:25 +0900

    Post delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_post, only: [:show]
+  before_action :find_post, only: [:show, :destroy]
 
   def index
     @posts = Post.all.limit(10).includes(:photos, :user).order('created_at DESC')
@@ -25,6 +25,19 @@ class PostsController < ApplicationController
       flash[:alert] = "Something went wrong ..."
       redirect_to posts_path
     end
+  end
+
+  def destroy
+    if @post.user == current_user
+      if @post.destroy
+        flash[:notice] = "Post deleted!"
+      else
+        flash[:alert] = "Something went wrong ..."
+      end
+    else
+      flash[:notice] = "You don't have permission to do that!"
+    end
+    redirect_to root_path
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   validates :image, presence: true
 
   has_many :photos, dependent: :destroy #Postが削除されるとphotoも消える
+
+  def is_belongs_to? user
+    Post.find_by(user_id: user.id, id: id)
+  end
 end
