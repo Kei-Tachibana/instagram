5e6ec5b6f633494d6f614be9b447b602e16ac718
Keith-Tachibana <kei5kei@icloud.com>
2019-01-06 23:35:33 +0900

    Comment Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
 
   has_many :photos, dependent: :destroy #Postが削除されるとphotoも消える
   has_many :likes, ->{order("created_at DESC") }
+  has_many :comments, ->{order("created_at DESC") }
 
   def is_belongs_to? user
     Post.find_by(user_id: user.id, id: id)
