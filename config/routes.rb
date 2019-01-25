c82e839a54aebfae8feba4bb14051eb694819cf1
Kei-Tachibana <un.wackeik@gmail.com>
2019-01-25 21:47:12 +0900

    Bookmark Model

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :photos, only: [:create]
-    resources :likes, only: [:create, :destroy] , shallow: true
+    resources :likes, only: [:create, :destroy], shallow: true
+    resources :bookmarks, only: [:create, :destroy], shallow: true
   end
 end
