0ef904ebba847fe304cba42fe244b30bd20461b2
Kei-Tachibana <un.wackeik@gmail.com>
2019-01-27 22:21:39 +0900

    Search View

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: :true, length: {maximum: 50}
+
+  def self.search(term)
+    if term
+      where('name LIKE ?', "%#{term}%")
+    else
+      nil
+    end
+  end
 end
