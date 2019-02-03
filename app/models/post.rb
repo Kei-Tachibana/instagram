3f28d294d903c18826770cd9c1ef82f08ba2e24f
Kei-Tachibana <un.wackeik@gmail.com>
2019-02-03 20:27:28 +0900

    Fix foreign key for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
-
-  validates :image, presence: true
-
   has_many :photos, dependent: :destroy #Postが削除されるとphotoも消える
-  has_many :likes, -> {order("created_at DESC") }
-  has_many :comments, -> {order("created_at DESC") }
+  has_many :likes, -> {order("created_at DESC") }, dependent: :destroy
+  has_many :comments, -> {order("created_at DESC") }, dependent: :destroy
   has_many :bookmarks
 
   def is_belongs_to? user
