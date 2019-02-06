52d8457a4f453304a5e4a875362d5cfce0240282
Kei-Tachibana <un.wackeik@gmail.com>
2019-02-06 23:32:59 +0900

    Now user_profile page looks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @users = User.search(params[:term])
@@ -8,6 +9,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.includes(:photos, :likes, :comments)
-    @saved = Post.joins(:bookmarks).where(user_id: current_user.id).includes(:photos, :likes, :comments) if @user == current_user
+    @saved = Post.joins(:bookmarks).where("bookmarks.user_id=?", current_user.id).includes(:photos, :likes, :comments) if @user == current_user
   end
 end
